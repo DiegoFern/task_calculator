6a5420475907290ca0f714049f871a3fb5d72bc6
Diego <diego130892@gmail.com>
2020-11-09 12:40:36 +0100

    fix compile error of dot

--- dot_Plugin.py
+++ dot_Plugin.py
@@ -8,7 +8,7 @@ class Plugin_to_dot(Plugin):
         def print(x):
             print._ans+=x+'\n'
         print._ans = ''
-        print('digraph{')
+        print('digraph G {')
         for node in Report.nodes:
             print(Plugin_to_dot.print_node(node))
             for g in (Plugin_to_dot.print_edge(node)):
@@ -19,7 +19,10 @@ class Plugin_to_dot(Plugin):
 
 
     def print_node(node):
-        return ''' n_{n}[shape=square, label={f} color={color},style=filled ]; '''.format(
+        return ''' n_{n}[shape=square, label={f}, color={color},style=filled,
+                    href='/n_{f}'
+    ]; '''.format(
+
                    f=str(node.function.__name__),
                    n=node.id_node,
                    color='green' if node.calculated else 'red'
